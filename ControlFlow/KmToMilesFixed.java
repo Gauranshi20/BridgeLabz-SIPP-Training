b8cec795c523dbc91cf375364f873db357eb1fc3
Gauranshi20 <srivastavagauranshi40@gmail.com>
2025-06-16 09:59:04 +0530

    [Gauranshi Srivastava].Add Control Flow Problems

--- ControlFlow/KmToMilesFixed.java
+++
@@ -1,8 +0,0 @@
-public class KmToMilesFixed {
-    public static void main(String[] args) {
-        double km = 10.8;
-        double miles = km / 1.6;
-
-        System.out.printf("The distance %.1f km in miles is %.2f miles%n", km, miles);
-    }
-}
